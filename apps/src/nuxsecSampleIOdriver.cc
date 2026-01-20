695bf5b19243c25ed6b0da2cd73044bdd4e203d6
Nicholas <107313324+njlane314@users.noreply.github.com>
2026-01-20 14:59:33 +0000

    Rename IO aggregator drivers

--- apps/src/nuxsecSampleIOdriver.cc
+++ apps/src/nuxsecSampleIOdriver.cc
@@ -1,6 +1,6 @@
 /* -- C++ -- */
 /**
- *  @file  apps/src/nuxsecSampleIOaggregator.cc
+ *  @file  apps/src/nuxsecSampleIOdriver.cc
  *
  *  @brief Main entrypoint for Sample aggregation.
  */
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
         {
             const std::vector<std::string> args = nuxsec::app::collect_args(argc, argv);
             const nuxsec::app::SampleArgs sample_args =
-                nuxsec::app::parse_sample_args(args, "Usage: nuxsecSampleIOaggregator NAME:FILELIST");
-            return nuxsec::app::run_sample(sample_args, "nuxsecSampleIOaggregator");
+                nuxsec::app::parse_sample_args(args, "Usage: nuxsecSampleIOdriver NAME:FILELIST");
+            return nuxsec::app::run_sample(sample_args, "nuxsecSampleIOdriver");
         });
 }

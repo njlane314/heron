18f703536f1c1905c9e7503396c6b0038bf1113c
Nicholas <107313324+njlane314@users.noreply.github.com>
2026-02-12 13:05:39 +0000

    Tighten default MIPness squashing separation

--- plot/macro/plotParticleLevelMipnessSelectionStacked.C
+++ plot/macro/plotParticleLevelMipnessSelectionStacked.C
@@ -61,8 +61,8 @@ int plotParticleLevelMipnessSelectionStacked(const std::string &event_list_path 
                                              bool include_data = true,
                                              const std::string &selection_upto = "sel_trigger && sel_triggered_slice && sel_reco_fv",
                                              const std::string &particle_pdg_branch = "backtracked_pdg_codes",
-                                             double squash_midpoint = 0.65,
-                                             double squash_softness = 0.12)
+                                             double squash_midpoint = 0.72,
+                                             double squash_softness = 0.06)
 {
     ROOT::EnableImplicitMT();
 

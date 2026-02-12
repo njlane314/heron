b0be65f4c352dd8551802e475a2b4cca39289ff1
Nicholas <107313324+njlane314@users.noreply.github.com>
2026-02-12 12:52:09 +0000

    Tune default MIPness squash midpoint for right-shifted muons

--- plot/macro/plotParticleLevelMipnessSelectionStacked.C
+++ plot/macro/plotParticleLevelMipnessSelectionStacked.C
@@ -61,7 +61,7 @@ int plotParticleLevelMipnessSelectionStacked(const std::string &event_list_path 
                                              bool include_data = true,
                                              const std::string &selection_upto = "sel_trigger && sel_triggered_slice && sel_reco_fv",
                                              const std::string &particle_pdg_branch = "backtracked_pdg_codes",
-                                             double squash_midpoint = 1.0,
+                                             double squash_midpoint = 0.65,
                                              double squash_softness = 0.2)
 {
     ROOT::EnableImplicitMT();

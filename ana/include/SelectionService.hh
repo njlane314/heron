d1cc46a31e1cc3102c27ab602c4f6accd3ed3c63
Nicholas <107313324+njlane314@users.noreply.github.com>
2026-02-19 14:28:11 +0000

    Consolidate SelectionService apply into single API

--- ana/include/SelectionService.hh
+++ ana/include/SelectionService.hh
@@ -51,16 +51,16 @@ class SelectionService
     static const float muon_max_track_distance;
     static const unsigned muon_required_generation;
 
-    static ROOT::RDF::RNode apply(ROOT::RDF::RNode node, Preset p);
+    static ROOT::RDF::RNode apply(ROOT::RDF::RNode node, Preset p, SelectionEntry *selection = NULL);
     static ROOT::RDF::RNode decorate(ROOT::RDF::RNode node);
     static std::string selection_label(Preset p);
     static bool is_in_truth_volume(float x, float y, float z) noexcept;
     static bool is_in_reco_volume(float x, float y, float z) noexcept;
 };
 
-inline ROOT::RDF::RNode apply(ROOT::RDF::RNode node, Preset p)
+inline ROOT::RDF::RNode apply(ROOT::RDF::RNode node, Preset p, SelectionEntry *selection = NULL)
 {
-    return SelectionService::apply(node, p);
+    return SelectionService::apply(node, p, selection);
 }
 
 

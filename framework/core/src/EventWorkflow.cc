20c1b58c92286564e87274c4141f2d39ceef739a
Nicholas <107313324+njlane314@users.noreply.github.com>
2026-02-21 23:55:53 +0000

    Fix EventWorkflow EventListIO namespace compile error

--- framework/core/src/EventWorkflow.cc
+++ framework/core/src/EventWorkflow.cc
@@ -17,6 +17,7 @@
 #include "ColumnDerivationService.hh"
 #include "EventCLI.hh"
 #include "EventColumnProvider.hh"
+#include "EventListIO.hh"
 #include "EventSampleFilterService.hh"
 #include "RDataFrameService.hh"
 #include "StatusMonitor.hh"

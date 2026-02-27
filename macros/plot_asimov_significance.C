620d71bb0b795a61295783b6f0b10939b58ced44
Nicholas <107313324+njlane314@users.noreply.github.com>
2026-02-27 15:18:05 +0000

    Fix ambiguous event-list helper in plotting macros

--- macros/plot_asimov_significance.C
+++ macros/plot_asimov_significance.C
@@ -23,28 +23,11 @@ R__ADD_INCLUDE_PATH(framework/modules/plot/include)
 #include "SelectionService.hh"
 #include "EventListIO.hh"
 #include "PlotEnv.hh"
+#include "PlottingHelper.hh"
 
 using namespace nu;
 
 namespace {
-
-bool looks_like_event_list_root(const std::string& path) {
-    const auto n = path.size();
-    if (n < 5 || path.substr(n - 5) != ".root") {
-        return false;
-    }
-
-    std::unique_ptr<TFile> input_file(TFile::Open(path.c_str(), "READ"));
-    if (!input_file || input_file->IsZombie()) {
-        return false;
-    }
-
-    const bool has_refs = (input_file->Get("sample_refs") != nullptr);
-    const bool has_events_tree = (input_file->Get("events") != nullptr);
-    const bool has_event_tree_key = (input_file->Get("event_tree") != nullptr);
-
-    return has_refs && (has_events_tree || has_event_tree_key);
-}
 
 std::shared_ptr<const std::vector<char>> build_truth_mc_mask(const EventListIO& event_list) {
     const auto& refs = event_list.sample_refs();
